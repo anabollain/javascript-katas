1f9cd647d27947c4728897f316b140ead885e286
Ana Bollain <ana.bollain@gmail.com>
2023-02-18 11:57:23 +0100

    Upload exercise01 & Readme

--- README.md
+++ README.md
@@ -1,2 +1,19 @@
-# javascript-katas
-Katas - Practice JavaScript coding with code challenges
+# JavaScript Katas
+
+Hey there! 👋
+
+I've created this repository to gather all JS Katas and challenges that I will be doing. Most of them come from Code Wars, an incredible platform to practice your programming skills. 
+
+I highly encourage you to try it if you haven't done it yet 😊
+
+* [Code Wars](https://www.codewars.com/)
+
+Each exercise will consist on a JS file, with the name and description on the top. I will be uploading the different solutions I come up with, as well as the official best voted solution available. 
+
+In some cases, I will add comments that I have found useful to help me understand some of the steps. 
+
+Hope you enjoy it, don't hesitate to contact me if you want to practice together or if I'm able to help you in any way!
+
+Let's get coding ✨
+
+
---
+++ exercise01_smartSum.js
@@ -0,0 +1,61 @@
+//Smart Sum
+
+//Description
+
+/*Your task is to write a function, smartSum, that returns the sum of an arbitrary number of arguments. But be careful, some of your arguments can be arrays of numbers or nested arrays.
+
+smartSum(1,2,3,[4,5],6); // returns 21
+smartSum(1,2,[[3,4],5],6); // returns 21
+*/
+
+
+//SOLUTION 01
+
+function smartSum(){
+  let sum = 0;
+  for(let i=0; i<arguments.length; i++){
+    if(typeof arguments[i] === 'object'){
+        sum += smartSum(...arguments[i]);
+    }else{
+        sum += arguments[i];
+    }
+  }
+  return sum;
+}
+
+//SOLUTION 02
+
+function smartSum() {
+  let totalSum = 0;
+  for(num of arguments){
+    if(typeof num === 'number'){
+        totalSum += num;
+    }else{
+        totalSum += smartSum(...num);
+        //if totalSum += smartSum(num); => then arguments = [[4,5]], infinite loop
+    }
+  }
+  return totalSum;
+}
+
+smartSum(1,2,3,[4,5],6);
+
+//Spread operator examples
+//smartSum(...[4,5]); => smartSum(4,5);
+
+//arr = [4,5]
+//arr2 = [...arr, 3] => arr2 = [...[4,5], 3] => arr2 = [4, 5, 3]
+
+//SOLUTION 03
+
+function smartSum() {
+  return [...arguments].reduce((a, b) => a + (typeof(b) === 'number' ? b : smartSum(...b)), 0);
+}
+
+
+//VOTED BEST SOLUTION
+
+const smartSum = (...a) => a.reduce((s, e) => s + (typeof e === "number" ? e : smartSum(...e)), 0);
+
+
+
